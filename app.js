e3cfe4720e9e6b0ce860db4f301eb7143bb7bf53
Atsushi <atsushi@moongift.jp>
2018-07-24 12:09:47 +0900

    Init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const talk = new OpenJTalk();
 const filePath = '/sys/class/thermal/thermal_zone0/temp';
 setInterval(() => {
   fs.readFile(filePath, 'utf-8', (err, data) => {
-    const temp = parseInt(data) / 1000;
+    const temp = parseInt(parseInt(data) / 1000);
     talk.talk(`現在の温度は${temp}です`);
   });
 }, 10000);
