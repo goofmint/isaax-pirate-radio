a7e0749fa5bb061da27d146b48363b7f3f5350b9
Atsushi <atsushi@moongift.jp>
2018-07-24 11:33:12 +0900

    Init

--- app.js
+++ app.js
@@ -3,22 +3,4 @@ const fs = require('fs');
 
 const talk = new OpenJTalk();
 
-let old_message = '';
-const filePath = './isaax-project.env';
-setInterval(() => {
-  try {
-    fs.statSync(filePath);
-  } catch (e) {
-    console.log(`File not found. ${filePath}`);
-    console.log(process.env);
-    return;
-  }
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    const match = data.match(/^MESSAGE="(.*)"/);
-    const message = match[1];
-    if (message !== old_message) {
-      talk.talk(message);
-      old_message = message;
-    }
-  });
-}, 5000);
+talk.talk(process.env.MESSAGE);
